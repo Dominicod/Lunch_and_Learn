bb8aa8ca98edfcacbf89a1e49ceace5d910bc9a5
dominicod <dominicodonnell99@gmail.com>
2022-11-10 17:17:22 -0600

    Feat: add uniq index on DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_225925) do
+ActiveRecord::Schema.define(version: 2022_11_10_231609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_11_10_225925) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "favorites", "users"
