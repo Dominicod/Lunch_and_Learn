b746896672744d8082e76d0613eb52177e5b0747
dominicod <dominicodonnell99@gmail.com>
2022-11-10 17:29:24 -0600

    Feat: create factories

---
+++ spec/factories/favorite.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :favorite do
+    country { Faker::Games::Minecraft.biome }
+    recipe_link { Faker::Internet.url }
+    recipe_title { Faker::Games::Minecraft.achievement }
+  end
+end
