ee26b1ee9fdbf22dcb0fcb69b417c9f3259c90dc
dominicod <dominicodonnell99@gmail.com>
2022-11-13 13:15:38 -0600

    Feat: error handling logic in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+  private
+
+  def render_unprocessable_entity(exception)
+    render json: unprocessable_entity(exception), status: :unprocessable_entity
+  end
+
+  def unprocessable_entity(exception)
+    errors = exception.message.split(':')[1].split(',').map(&:strip)
+    ErrorSerializer.new(Error.new(code: '422', status: Rack::Utils::HTTP_STATUS_CODES[422], message: errors))
+  end
 end
