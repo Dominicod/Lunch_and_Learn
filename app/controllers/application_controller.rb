9b941849900eddc7395d86802e05cd883752d3f3
dominicod <dominicodonnell99@gmail.com>
2022-11-13 13:39:09 -0600

    Feat: add logic to error in appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActionController::ParameterMissing, with: :render_bad_request
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   private
 
@@ -9,10 +11,30 @@ class ApplicationController < ActionController::API
     render json: unprocessable_entity(exception), status: :unprocessable_entity
   end
 
+  def render_bad_request(exception)
+    render json: bad_request(exception), status: :bad_request
+  end
+
+  def render_not_found(exception)
+    render json: not_found(exception), status: :not_found
+  end
+
+  def not_found(exception)
+    error_message({ code: 404, status: Rack::Utils::HTTP_STATUS_CODES[404], message: exception })
+  end
+
+  def bad_request(exception)
+    error_message({ code: 400, status: Rack::Utils::HTTP_STATUS_CODES[400], message: exception })
+  end
+
   def unprocessable_entity(exception)
     errors = exception.message.split(':')[1].split(',').map(&:strip)
+    error_message({ code: 422, status: Rack::Utils::HTTP_STATUS_CODES[422], message: errors })
+  end
+
+  def error_message(error)
     ErrorSerializer.new(
-      Error.new({ code: 422, status: Rack::Utils::HTTP_STATUS_CODES[422], message: errors })
+      Error.new({ code: error[:code], status: error[:status], message: error[:message] })
     ).serializable_json
   end
 end
