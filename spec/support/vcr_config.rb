036fd62ed043311b0ce68158e776dac944c87db3
dominicod <dominicodonnell99@gmail.com>
2022-11-12 20:45:34 -0600

    FixL VCR bug

--- spec/support/vcr_config.rb
+++ spec/support/vcr_config.rb
@@ -5,8 +5,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.default_cassette_options = { re_record_interval: 7.days }
-  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['ED_APP_ID'] }
-  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['ED_APP_KEY'] }
-  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['GOOGLE_API_KEY'] }
-  config.filter_sensitive_data('DONT SHOW MY API KEY') { ENV['UNSPLASH_CLIENT'] }
+  config.filter_sensitive_data('ED_APP_ID') { ENV['ED_APP_ID'] }
+  config.filter_sensitive_data('ED_APP_KEY') { ENV['ED_APP_KEY'] }
+  config.filter_sensitive_data('GOOGLE_API_KEY') { ENV['GOOGLE_API_KEY'] }
+  config.filter_sensitive_data('UNSPLASH_CLIENT') { ENV['UNSPLASH_CLIENT'] }
 end
