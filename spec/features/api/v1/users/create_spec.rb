607c7f12386c46d21cf34126c643dbeb3a7ea78c
dominicod <dominicodonnell99@gmail.com>
2022-11-13 13:12:13 -0600

    Test: create sadpath test

--- spec/features/api/v1/users/create_spec.rb
+++ spec/features/api/v1/users/create_spec.rb
@@ -38,5 +38,40 @@ RSpec.describe 'Users | Create', type: :request do
         end
       end
     end
+    context('Sad Path') do
+      describe 'and I send in the correct request body attributes' do
+        let(:user) do
+          {
+            "name": 'Athena Dao',
+            "email": 'athenadao@bestgirlever.com',
+            "password": 'password',
+            "password_confirmation": 'password'
+          }
+        end
+        let(:headers) { { CONTENT_TYPE: 'application/json' } }
+
+        it "doesn't create a new user if email already exists" do
+          post api_v1_users_path, headers: headers, params: JSON.generate(user: user)
+
+          expect do
+            post api_v1_users_path, headers: headers, params: JSON.generate(user: user)
+          end.to change(User, :count).by(0)
+        end
+
+        it 'returns correct error message' do
+          post api_v1_users_path, headers: headers, params: JSON.generate(user: user)
+          post api_v1_users_path, headers: headers, params: JSON.generate(user: user)
+
+          expect(response).to have_http_status :unprocessable_entity
+
+          error_response = JSON.parse(response.body, symbolize_names: true)
+
+          expect(error_response).to have_key :errors
+          expect(error_response.dig(:errors, :status)).to eq 'Unprocessable Entity'
+          expect(error_response.dig(:errors, :message)).to eq 'Validation failed: Email has already been taken'
+          expect(error_response.dig(:errors, :code)).to eq 422
+        end
+      end
+    end
   end
 end
