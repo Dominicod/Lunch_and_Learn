69b834515810c4c736fecec11442f67f48124a26
dominicod <dominicodonnell99@gmail.com>
2022-11-12 18:41:00 -0600

    Feat: create video facade

--- app/objects/learning_resource.rb
+++ app/objects/learning_resource.rb
@@ -9,10 +9,7 @@ class Recipe # rubocop:todo Style/Documentation
   def initialize(images, video, query)
     @id = nil
     @country = query
-    @video = {
-      title: video[:snippet][:title],
-      youtube_video_id: video[:id][:videoId]
-    }
+    @video = []
     @images = []
   end
 end
