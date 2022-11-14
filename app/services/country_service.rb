400e5720056581050b638589766c6f8c99e66d8d
dominicod <dominicodonnell99@gmail.com>
2022-11-14 10:50:30 -0600

    Feat: add lat long conn to country service

--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -6,6 +6,10 @@ class CountryService # rubocop:todo Style/Documentation
     parse(conn.get('/v3.1/all?fields=name'))
   end
 
+  def self.country_lat_long(country)
+    parse(conn.get("/v3.1/name/#{ServiceHelperModule.encode_uri(country)}"))
+  end
+
   def self.parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
--- app/services/tourist_sight_service.rb
+++ app/services/tourist_sight_service.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class TouristSightService # rubocop:todo Style/Documentation
-  include ServiceHelperModule
   def self.sights(long, lat)
     parse(conn.get("?categories=tourism.sights&filter=circle:#{long},#{lat},20000"))
   end
