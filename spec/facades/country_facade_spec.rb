df18dc397cc897cf5517c58778e92a7cb8475a5d
dominicod <dominicodonnell99@gmail.com>
2022-11-14 11:04:20 -0600

    Test: new facade method

--- spec/facades/country_facade_spec.rb
+++ spec/facades/country_facade_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe CountryFacade, :vcr, type: :facade do
       expect(described_class.random_country).to be_an_instance_of Country
     end
   end
+
+  describe '.country_lat_long' do
+    it "creates a object with a country's lat/long" do
+      expect(described_class.country_lat_long('france')).to be_an_instance_of CountryLatLong
+    end
+  end
 end
