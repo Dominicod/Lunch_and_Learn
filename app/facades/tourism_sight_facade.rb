5728254acae1fb83b01ba542c3f02626a4786dc7
dominicod <dominicodonnell99@gmail.com>
2022-11-14 10:44:11 -0600

    Test: create test for facade

---
+++ app/facades/tourism_sight_facade.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class TourismSightFacade # rubocop:todo Style/Documentation
+  def self.create_sights(query)
+
+  end
+end
---
+++ spec/facades/tourism_sight_facade_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TourismSightFacade, :vcr, type: :facade do
+  describe '.create_video' do
+    it 'returns an array' do
+      expect(described_class.create_sights('France')).to be_an Array
+    end
+
+    it 'returns an array of sight objects' do
+      expect(described_class.create_sights('France')).to all(be_an_instance_of(Sight))
+    end
+
+    it 'returns empty array if search results are zero' do
+      expect(described_class.create_sights('Ecuador')).to eq []
+    end
+  end
+end
