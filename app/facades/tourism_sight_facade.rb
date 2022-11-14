d462b272c99705c0403f8d883adcc391082c2241
dominicod <dominicodonnell99@gmail.com>
2022-11-14 11:07:56 -0600

    Feat: create sight facade logic

--- app/facades/tourism_sight_facade.rb
+++ app/facades/tourism_sight_facade.rb
@@ -2,6 +2,9 @@
 
 class TourismSightFacade # rubocop:todo Style/Documentation
   def self.create_sights(query)
+    obj = CountryFacade.country_lat_long(query)
+    sights_response = TouristSightService.sights(obj.long, obj.lat)
 
+    sights_response[:features].map { |sight| Sight.new(sight) }
   end
 end
