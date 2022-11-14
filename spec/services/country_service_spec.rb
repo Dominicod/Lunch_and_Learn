48142217dc77fba5da37002ed98f7fabc8173c3c
dominicod <dominicodonnell99@gmail.com>
2022-11-14 10:56:55 -0600

    Feat: test new service method

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe CountryService, :vcr, type: :service do
-  let(:response) { described_class.country_list }
 
   describe '.country_list' do
+    let(:response) { described_class.country_list }
+
     it 'returns as an array' do
       expect(response).to be_an Array
     end
@@ -16,6 +17,30 @@ RSpec.describe CountryService, :vcr, type: :service do
 
     it 'returns official name as string' do
       expect(response[0][:name][:official]).to be_an String
+    end
+  end
+
+  describe '.country_lat_long' do
+    let(:response) { described_class.country_lat_long('france') }
+
+    it 'returns as an array' do
+      expect(response).to be_an Array
+    end
+
+    it 'returns capital info as a hash' do
+      expect(response[0][:capitalInfo]).to be_an Hash
+    end
+
+    it 'returns latlng as an array' do
+      expect(response[0][:capitalInfo][:latlng]).to be_an Array
+    end
+
+    it 'returns lat as an float' do
+      expect(response[0][:capitalInfo][:latlng][0]).to be_an Float
+    end
+
+    it 'returns long as an float' do
+      expect(response[0][:capitalInfo][:latlng][1]).to be_an Float
     end
   end
 end
