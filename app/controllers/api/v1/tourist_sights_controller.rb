bcc070bd9283cda5428acde2ae6c144d6fb529d5
dominicod <dominicodonnell99@gmail.com>
2022-11-14 10:17:20 -0600

    Feat: create controller

---
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TouristSightsController < ApplicationController # rubocop:todo Style/Documentation
+      def index
+
+      end
+    end
+  end
+end
